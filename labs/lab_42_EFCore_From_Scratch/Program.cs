2023b37cd665eba6b62f31796a7c10a3bb80d73b
Ryan-Paul-Burdus <53583747+Ryan-Paul-Burdus@users.noreply.github.com>
2019-10-04 13:04:46 +0100

    added screenshots

--- labs/lab_42_EFCore_From_Scratch/Program.cs
+++ labs/lab_42_EFCore_From_Scratch/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace lab_42_EFCore_From_Scratch
 {
     class Program
     {
+        static List<ToDoItem> todos = new List<ToDoItem>();
         static void Main(string[] args)
         {
             using (var db = new ToDoItemContext())
@@ -17,6 +20,13 @@ namespace lab_42_EFCore_From_Scratch
                 db.ToDoItems.Add(todo);
                 db.SaveChanges();
             }
+
+            System.Threading.Thread.Sleep(1000);
+            using (var db = new ToDoItemContext())
+            {
+                todos = db.ToDoItems.ToList();
+            }
+            todos.ForEach(todo => Console.WriteLine($"{todo.ToDoItemId, -10}{todo.DateCreated, -20}{todo.ToDoItemName}"));
         }
     }
 
